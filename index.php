aa5d5d0781265775ecf2494f245f3046a528c5d6
Jason Giancono <jasongiancono@gmail.com>
2016-04-12 23:07:46 +0800

    added license

--- index.php
+++ index.php
@@ -1,9 +1,24 @@
 <!DOCTYPE html>
 <?php
-ini_set('display_errors', 'On');
-error_reporting(E_ALL | E_STRICT);
+//debug
+//ini_set('display_errors', 'On');
+//error_reporting(E_ALL | E_STRICT);
 ?>
 <?php
+/* PHP MusicPlayer
+/* By Jason Giancono
+/* jasongi.com
+/*  
+    Copyright (C) 2016 Jason Giancono (jasongiancono@gmail.com)
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+*/
 
 function getID3($filename)
 {
